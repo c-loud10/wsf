9d76d97b48f2725494e08200bc86a566089d66f3
P19o <king.medo7200@gmail.com>
2019-04-03 01:04:47 +0200

    --add now

--- ep/index.php
+++ ep/index.php
@@ -1,7 +1,10 @@
 <?php
 
 
-echo("it is the end point");
+$xml = file_get_contents("https://maker.ifttt.com/trigger/req/with/key/nGOZsEJ3havjIuPITpF1CwxRnZGzc79BlkZE4Ps9IMS");
+
+
+echo("it is the end point" . $xml);
 
 
 ?>
